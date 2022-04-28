ae59078703b736804d9bd6f580dc1d7f47956e6b
francamaradev <francamaradev@gmail.com>
2022-04-28 09:00:56 -0300

    feat: basic header

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ export default function Nav() {
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>flemaForums</Box>
+          <Box>Flema Log</Box>
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
